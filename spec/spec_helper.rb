e961efdc1e8f1488b3276cf76d9378d4b0af5870
github username <github email address>
2017-08-08 10:56:14 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 require 'rspec'
 require 'capybara/rspec'
 require 'capybara/dsl'
+require 'spec_helper'
 
 # Configure RSpec
 RSpec.configure do |config|
@@ -18,3 +19,30 @@ end
 
 # Configure Capybara to test against the application above.
 Capybara.app = app
+
+
+describe "GET '/' - Greeting Form" do
+  it 'welcomes the user' do
+    visit '/'
+    expect(page.body).to include("Welcome!")
+  end
+
+    # New test
+  it 'has a greeting form with a user_name field' do
+    visit '/'
+
+    expect(page).to have_selector("form")
+    expect(page).to have_field(:user_name)
+  end
+end
+
+describe "POST '/greet' - User Greeting" do
+  it 'greets the user personally based on their user_name in the form' do
+    visit '/'
+
+    fill_in(:user_name, :with => "Avi")
+    click_button "Submit"
+
+    expect(page).to have_text("Hi Avi, nice to meet you!")
+  end
+end
